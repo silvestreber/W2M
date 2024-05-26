8fe19ad60c1e8f2aeb4691a4ba38675be550ce1d
silvestreber <silvestrebermejo@gmail.com>
2024-05-26 18:23:29 +0200

    Se añaden scripts inicialización datos

---
+++ src/main/resources/db/inicialization/V1__Create_nave_espacial_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS nave_espacial (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    aparicion VARCHAR(255) NOT NULL,
+    tipo VARCHAR(255) NOT NULL
+);
---
+++ src/main/resources/db/inicialization/V2__Insert_some_initial_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO nave_espacial (nombre, aparicion, tipo) VALUES
+('Halcón Milenario', 'Star Wars', 'Película'),
+('Endurance', 'Interstellar', 'Película'),
+('USS Enterprise', 'Star Trek', 'Serie'),
+('Nave nodriza', 'District 9', 'Película'),
+('Nave Nodriza', 'Encuentros en la tercera fase', 'Película'),
+('Platillo Volante', 'Independence Day', 'Película'),
+('Discovery One', '2001: Una Odisea del Espacio', 'Película'),
+('Derelict', 'Alien, Prometheus', 'Película'),
+('Serenity', 'Firefly', 'Serie'),
+('Nave de los heptapods', 'Arrival', 'Película');
